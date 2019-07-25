a5b406f08579dc0011edc8c454b22de520e300dc
Johannes Reker <j.reker@ktr.com>
2019-07-25 10:27:47 +0200

    - added menu - added function that you can show mfa settings of specific user

--- exportO365MFASettings.ps1
+++ exportO365MFASettings.ps1
@@ -14,7 +14,7 @@ $global:credential = $null
 
 ##### USER INPUTS
 write-host "--Office 365 MFA-Settings exporter --"
-$CSVPath = Read-Host "Where do you want to store the CSV file? (eg. C:\temp\export.csv)"
+
 
 #Function that checks if you are already connected to Office 365 
 function connectToMSO() {
@@ -28,12 +28,19 @@ function connectToMSO() {
     } 
 }
 
-function getResult() {
+function getResult($userUPN) {
     try{
         $result = New-Object Collections.Generic.List[PSObject]
         #Get All MSOL Users
         write-host "Start to export user settings...please wait..."
-        foreach($user in Get-MsolUser -All -ErrorAction Stop) {
+        $users = ""
+        if($null -eq $userUPN) {
+            $users = Get-MsolUser -All -ErrorAction Stop
+        } else {
+            $users = Get-MsolUser -UserPrincipalName $userUPN
+        }
+        
+        foreach($user in $users) {
             
             $MFA_STATE = $null
             if($null -eq $user.StrongAuthenticationRequirements.State){ 
@@ -68,6 +75,49 @@ function getResult() {
 
 #connect to microsoft with office 365 credentials
 connectToMSO
+
+
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Office 365 # MFA Export'
+     )
+     Clear-Host
+     Write-Host "================ $Title ================"
+     Write-Host "1: Press '1' to export MFA settings of all users to csv."
+     Write-Host "2: Press '2' to show settings of specific user." 
+     Write-Host "Q: Press 'Q' to quit."
+}
+
+
+
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                Clear-Host
+                Write-Host "==> 1: export MFA settings of all users."
+                $CSVPath = Read-Host "Where do you want to store the CSV file? (eg. C:\temp\export.csv)"
+                getResult | Export-Csv -Path $CSVPath -NoClobber -Delimiter ';'
+           } '2'  {
+                Clear-Host
+                Write-Host "==> 2:show M FA settings of specific user"
+                $userUPN = read-host "UPN of user"
+                getResult $userUPN
+           } 'q' {
+            return
+       }
+ }
+ pause
+}
+until ($input -eq 'q')
+
+
+
+
 #export results to csv
-getResult | Export-Csv -Path $CSVPath -NoClobber -Delimiter ';'
+
 
